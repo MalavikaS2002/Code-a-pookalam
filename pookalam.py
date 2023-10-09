632810566e87f189bdd759e0e28b5a9c77c1f395
MalavikaS2002 <118829603+MalavikaS2002@users.noreply.github.com>
2023-10-09 21:53:52 +0530

    Add files via upload

---
+++ pookalam.py
@@ -0,0 +1,39 @@
+c=circle(r=147,fill="red")
+c0=circle(r=150,fill="yellow")
+c1 = circle(r=150,fill="white")
+c2=circle(r=145,fill="black",stroke="#ae4540",stroke_width=10)
+
+r1=rectangle(w=190,h=190,fill="#ffa500",stroke="red")|repeat(15,rotate(50))
+r2=rectangle(w=180,h=180,fill="brown",stroke="brown")|rotate(15)|repeat(15,rotate(50))
+r3=rectangle(w=170,h=170,fill="yellow",stroke="green")|rotate(0)|repeat(15,rotate(50))
+r4=rectangle(w=160,h=160,fill="crimson",stroke="orange")|rotate(15)|repeat(15,rotate(50))
+r5=rectangle(w=150,h=150,fill="yellow",stroke="yellow")|rotate(0)|repeat(15,rotate(50))
+r6=rectangle(w=140,h=140,fill="white",stroke="white")|rotate(15)|repeat(15,rotate(50))
+
+s1=circle(r=100,fill="black",stroke="none")
+s2=circle(r=95,fill="#045504",stroke="none")
+s3=circle(r=90,fill="#057405",stroke="none")
+s4=circle(r=85,fill="#0c910c",stroke="none")
+s5=circle(r=80,fill="yellow",stroke="none")
+
+p1=rectangle(w=130,h=130,fill="red",stroke="none")|rotate(15)|repeat(300,rotate(50))
+p2=rectangle(w=120,h=120,fill="green",stroke="none")|rotate(0)|repeat(10,rotate(50))
+p3=rectangle(w=110,h=110,fill="maroon",stroke="none")|rotate(15)|repeat(10,rotate(50))
+p4=rectangle(w=100,h=100,fill="#e85300",stroke="none")|rotate(0)|repeat(10,rotate(50))
+p5=rectangle(w=90,h=90,fill="#56004f",stroke="none")|rotate(15)|repeat(10,rotate(50))
+
+q1=ellipse(w=10,h=90,fill="red")|repeat(200,rotate(5))
+q2=ellipse(w=10,h=80,fill="red",stroke="none")|repeat(200,rotate(10))
+q3=ellipse(w=30,h=70,fill="maroon",stroke="none")|repeat(200,rotate(10))
+d1=rectangle(w=50,h=50,fill="orange",stroke="none")|repeat(100,rotate(15))
+d2=rectangle(w=40,h=40,fill="red",stroke="none")|repeat(100,rotate(15))
+d3=rectangle(w=30,h=30,fill="brown",stroke="none")|repeat(100,rotate(15))
+q4=circle(r=15,fill="purple")
+q5=circle(r=10,fill="green")
+q6=circle(r=40,fill="#ffa200") 
+c8 = rectangle(w=65, h=65, fill="yellow") | repeat(200, rotate(20) | scale(0.9))
+c7 = rectangle(w=40, h=40, fill="#ff0000") | repeat(300, rotate(10) | scale(0.9))
+show(c,c0,c1,c2,r1,r2,r3,r4,r5,r6,s1,s2,s3,s4,p1,p2,p3,p4,p5,q1,q2,q3,d1,d2,d3,q4,q5,q6,c8,c7)
+
+              
+                                                                      
